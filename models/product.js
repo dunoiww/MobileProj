30c760da405e470305c63a40f2263b19f3d12b5b
huynhan03 <huynhnhandanang@gmail.com>
2023-06-20 20:38:00 +0700

    update model and page admin

--- models/product.js
+++ models/product.js
@@ -14,6 +14,38 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  screen: {
+    type: String,
+    required: true
+  },
+  operating_system: {
+    type: String,
+    required: true
+  },
+  rear_camera: {
+    type: String,
+    required: true
+  },
+  rear_camera: {
+    type: String,
+    required: true
+  },
+  ram: {
+    type: String,
+    required: true
+  },
+  rom: {
+    type: String,
+    required: true
+  },
+  chip: {
+    type: String,
+    required: true
+  },
+  sim: {
+    type: String,
+    required: true
+  },
   images: {
     type: Array,
     required: true
