6a625d23acfcfd64e72bafa3bae3eab33e2f1e64
Ngo Nam <namngo102003@gmail.com>
2023-06-24 00:38:06 +0700

    Update UI

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ import { getAllProduct, getSomeProduct, getProductBrand, getProductSortByPrice, 
 let mainCustomer = currentCustomer; // customer
 console.log(mainCustomer);
 
-let searchForm = document.querySelector('.header-page .search-form');
+let searchForm = document.querySelector('.search-form');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
 
 let navbar = document.querySelector('.header-page .navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
+    // searchForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
