6dc22ecb4f5ac584cd2988186e440e7e855188d6
Ngo Nam <namngo102003@gmail.com>
2023-06-23 21:54:49 +0700

    fix ui

--- controllers/const.js
+++ controllers/const.js
@@ -1,2 +1,3 @@
 //export var API_URL = "http://localhost:3000/"
-export var API_URL = "https://9a37-2402-800-6388-48f-39c0-eec2-aab7-edcf.ngrok-free.app"
+// export var API_URL = "https://9a37-2402-800-6388-48f-39c0-eec2-aab7-edcf.ngrok-free.app"
+export var API_URL = "https://23a0-2402-800-6388-48f-39c0-eec2-aab7-edcf.ngrok-free.app/"
